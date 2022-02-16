9b3d02367015d660a8143e913d55498c0b675529
Richard Harman <jordan28031@gmail.com>
2022-02-16 14:44:12 +0100

    Math library
    Creation of mathematical libraray. First commit.

---
+++ mathlib.py
@@ -0,0 +1,12 @@
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def devide(num1, num2):
+    return num1 / num2
+
