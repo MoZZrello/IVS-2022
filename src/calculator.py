2d25e87bed53e94ea484b0bb6ebafce63274c5cb
Marek Špirka <marek.spirka17@gmail.com>
2022-04-26 17:17:10 +0200

    Revert "Name and logo settings"
    This reverts commit 105b76bdb370ca14c750660b6df074a7b5d1642c.

--- src/calculator.py
+++ src/calculator.py
@@ -29,8 +29,7 @@ class Calculator_Window(QtWidgets.QMainWindow, window_calc):
         self.setupUi(self)
         self.show()
         self.setFixedSize(245, 495)
-        self.setWindowTitle("XOcalc")
-        self.setWindowIcon(QtGui.QIcon('calculator_logo.png'))
+        self.setWindowTitle("Calculator")
 
         self.button_number_zero.clicked.connect(lambda: self.pushed_number("0"))
         self.button_number_one.clicked.connect(lambda: self.pushed_number("1"))
